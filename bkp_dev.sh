2b97b269f0fc8d2949b74d29c55677bc62d78a7f
imM8rtal <igor.silva@problemcompany.org>
2023-06-24 16:12:08 -0300

    bkp scripts & faster sync

---
+++ bkp_dev.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# MySQL Connection Details
+DB_HOST="168.119.50.201"
+DB_USER="root"
+DB_PASS=""
+DB_PORT="3305"
+DB_NAME="kline"
+
+# Output file
+OUTPUT_FILE="bkp_dev.sql"
+
+# mysqldump -h 168.119.50.201 -u root -P 3305 -p kline > Documentos/CodeRepositories/klineService/bkp.sql
+# Function to perform the backup
+perform_backup() {
+    mysqldump -h $DB_HOST -u $DB_USER -P $DB_PORT -p $DB_NAME > $OUTPUT_FILE
+}
+
+# Function to import the backup
+perform_import() {
+    mysql -u igor -p123456 kline < $OUTPUT_FILE
+}
+
+# Function to handle reconnection
+handle_reconnection() {
+    while true; do
+        echo "Attempting to reconnect..."
+        perform_backup
+        if [ "$?" -eq 0 ]; then
+            echo "Backup completed successfully."
+            perform_import
+            if [ "$?" -eq 0 ]; then
+                echo "Import completed successfully."
+                break
+            else
+                echo "Import failed. Retrying in 5 seconds..."
+            fi
+        else
+            echo "Backup failed. Retrying in 5 seconds..."
+        fi
+        sleep 5
+    done
+}
+
+# Perform backup with reconnection handling
+handle_reconnection
--- job/syncKlineTable.job.go
+++ job/syncKlineTable.job.go
@@ -9,63 +9,70 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 func (j *Job) SyncKlineTable(db *database.Database) {
 	assets := j.Redis.GetCache("Assets", "string")
-
 	if assets == nil {
 		log.Panic("No assets found")
 	}
 
 	var cachedSlice []*asset.Asset
 	err := json.Unmarshal([]byte(assets.(string)), &cachedSlice)
-
 	if err != nil {
 		log.Panic("BBSKT Unmarshal: ", err)
 	}
 
 	log.Println("Syncronizing Klines")
+	var wg sync.WaitGroup
+	wg.Add(len(cachedSlice))
 
 	for _, a := range cachedSlice {
-		conn := db.CreateConnection()
+		go func(a *asset.Asset, wg *sync.WaitGroup) {
+			conn := db.CreateConnection()
 
-		fMts := j.KlineRepo.FindFirstMts(nil, conn, int64(a.Asset), 1, 2, j.Test)
-		conn.Close()
+			fMts := j.KlineRepo.FindFirstMts(nil, conn, int64(a.Asset), 1, 2, j.Test)
+			conn.Close()
 
-		if (fMts == kline.Kline{}) {
-			continue
-		}
-
-		start := time.UnixMilli(int64(fMts.Mts) / 1000)
-		end := start.Add(time.Hour * 5)
-
-		var count int64
-		for start.Before(time.Now()) {
-			j.request(db, fmt.Sprintf("%sUSDT", a.Symbol), a.Asset, start.UnixMilli(), end.UnixMilli(), 0)
-			count++
-
-			time.Sleep(time.Second)
-			start = start.Add(time.Hour * 5)
-			end = end.Add(time.Hour * 5)
-
-			if !start.Before(time.Now()) {
-				j.request(db, fmt.Sprintf("%sUSDT", a.Symbol), a.Asset, 0, end.UnixMilli(), 0)
-				count++
+			if (fMts == kline.Kline{}) {
+				wg.Done()
+				return
 			}
-		}
+			// fmt.Println(a)
+			start := time.UnixMilli(int64(fMts.Mts) / 1000)
+			end := start.Add(time.Hour * 5)
 
-		log.Printf("%sUSDT Klines Syncronization Looped %d times", strings.ToUpper(a.Symbol), count)
+			var count int64
+			for start.Before(time.Now()) {
+				j.request(db, fmt.Sprintf("%sUSDT", a.Symbol), a.Asset, start.UnixMilli(), end.UnixMilli(), 0)
+				count++
+				// fmt.Println(count)
+				time.Sleep(time.Second)
+				start = start.Add(time.Hour * 5)
+				end = end.Add(time.Hour * 5)
+				// fmt.Println(start, end)
+				if !start.Before(time.Now()) {
+					j.request(db, fmt.Sprintf("%sUSDT", a.Symbol), a.Asset, 0, end.UnixMilli(), 0)
+					count++
+				}
+			}
+
+			log.Printf("%sUSDT Klines Syncronization Looped %d times \n", strings.ToUpper(a.Symbol), count)
+			wg.Done()
+		}(a, &wg)
 	}
 
+	wg.Wait()
 	log.Println("Klines Syncronization is Finished")
 }
 
 func (j *Job) request(db *database.Database, symbol string, asset uint64, start, end, limit int64) {
-	resp, _ := j.ByBitInterface.GetKlines("spot", symbol, "1", start, end, limit)
+	resp, err := j.ByBitInterface.GetKlines("spot", symbol, "1", start, end, limit)
 
 	if len(resp) == 0 {
+		log.Println("SKTR 01: ", err)
 		return
 	}
 
